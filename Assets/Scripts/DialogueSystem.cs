6556c96e81e52e240f459a7389842ffff23ccc23
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-06 02:38:54 -0400

    Updated most of the prefabs

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -1,18 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro; // Import the TextMeshPro namespace
 
 public class DialogueSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class DialogueLine
     {
-        
+        public string characterName;
+        [TextArea(3, 5)]
+        public string line;
     }
 
-    // Update is called once per frame
+    public TMP_Text characterNameText;
+    public TMP_Text dialogueText;
+    public DialogueLine[] dialogueLines;
+    public string nextSceneName;
+    public float textSpeed = 0.05f;
+
+    private int currentLineIndex = 0;
+    private Coroutine displayCoroutine;
+
+    void Start()
+    {
+        ShowCurrentLine();
+    }
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (displayCoroutine != null)
+            {
+                StopCoroutine(displayCoroutine);
+                dialogueText.text = dialogueLines[currentLineIndex].line;
+            }
+            else
+            {
+                ShowNextCharacter();
+            }
+        }
+    }
+
+    void ShowCurrentLine()
+    {
+        if (currentLineIndex < dialogueLines.Length)
+        {
+            characterNameText.text = dialogueLines[currentLineIndex].characterName;
+            dialogueText.text = "";
+            displayCoroutine = StartCoroutine(AnimateText(dialogueLines[currentLineIndex].line));
+        }
+        else
+        {
+            // If there are no more dialogue lines, jump to the specified scene
+            JumpToScene(nextSceneName);
+        }
+    }
+
+    void ShowNextCharacter()
+    {
+        if (displayCoroutine != null)
+        {
+            StopCoroutine(displayCoroutine);
+            dialogueText.text = dialogueLines[currentLineIndex].line;
+        }
+        else
+        {
+            ShowNextLine();
+        }
+    }
+
+    void ShowNextLine()
+    {
+        currentLineIndex++;
+        ShowCurrentLine();
+    }
+
+    IEnumerator AnimateText(string line)
+    {
+        for (int i = 0; i < line.Length; i++)
+        {
+            dialogueText.text = line.Substring(0, i + 1);
+            yield return new WaitForSeconds(textSpeed);
+        }
+        displayCoroutine = null;
+    }
+
+    void JumpToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
